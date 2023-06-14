afd7dc10fcbbfaa70cf4a7a60a54981b52c4f6eb
Flo <florent.vouin@gmail.com>
2023-06-14 14:11:48 +0200

    ✨ Define the EventPublisher interface

---
+++ src/events/publisher.ts
@@ -0,0 +1,44 @@
+import { Event } from './event.js';
+
+/**
+ * A set of attributes that can be sent along with an event.
+ * This can help with filtering events or for deserialization. However, the content of the event itself should be enough
+ * to interpret and process it.
+ */
+export type EventAttributes = Record<string, string>;
+
+/**
+ * A set of options when publishing an event.
+ */
+export type PublishOptions = {
+  /**
+   * Attributes to send along with the event.
+   * This may not be supported by all publishers.
+   */
+  attributes?: EventAttributes;
+
+  /**
+   * The key used for partitioning or ordering the event.
+   * This may not be supported by all publishers.
+   */
+  key?: string;
+};
+
+/**
+ * A publisher of business events.
+ */
+export interface EventPublisher {
+  /**
+   * Flushes the publisher, such that all buffered messages are sent.
+   */
+  flush(): Promise<void>;
+
+  /**
+   * Publishes a new event.
+   *
+   * @param topic The topic to publish the event to.
+   * @param event The event to publish.
+   * @param options Additional publishing options.
+   */
+  publish(topic: string, event: Event, options?: PublishOptions): Promise<void>;
+}
