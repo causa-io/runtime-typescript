5dc0f654ecd4fb40db6da73a8a205ad665bf1f84
Flo <florent.vouin@gmail.com>
2023-06-14 16:31:59 +0200

    ✨ Define the VersionedEntity type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './errors/index.js';
 export * from './events/index.js';
 export * from './serialization/index.js';
 export * from './transaction/index.js';
+export * from './versioned-entity/index.js';
