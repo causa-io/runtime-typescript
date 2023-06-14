c38e9b76090a2b1d8c0314c7e727776ae6e169fb
Flo <florent.vouin@gmail.com>
2023-06-14 14:57:40 +0200

    ✨ Implement the BufferEventTransaction

---
+++ src/transaction/buffer-event-transaction.ts
@@ -0,0 +1,61 @@
+import { Event, EventPublisher, PublishOptions } from '../events/index.js';
+import { EventTransaction } from './event-transaction.js';
+
+/**
+ * An event staged as part of a transaction.
+ */
+type BufferedEvent = {
+  /**
+   * The topic to which the event should be published when transaction commits.
+   */
+  topic: string;
+
+  /**
+   * The event to publish.
+   */
+  event: Event;
+
+  /**
+   * Options for publishing the event.
+   */
+  options: PublishOptions;
+};
+
+/**
+ * An {@link EventTransaction} that buffers events locally until the transaction should be committed.
+ * All buffered events are forwarded to the given {@link EventPublisher} when the transaction commits.
+ * The transaction runner should call {@link BufferEventTransaction.commit} when the transaction commits.
+ */
+export class BufferEventTransaction implements EventTransaction {
+  /**
+   * The staged events that will be published when the transaction commits.
+   */
+  private readonly bufferedEvents: BufferedEvent[] = [];
+
+  /**
+   * Creates a new {@link BufferEventTransaction}.
+   *
+   * @param publisher The {@link EventPublisher} to which the buffered events will be passed when the transaction
+   *   commits.
+   */
+  constructor(readonly publisher: EventPublisher) {}
+
+  async publish(
+    topic: string,
+    event: Event,
+    options: PublishOptions = {},
+  ): Promise<void> {
+    this.bufferedEvents.push({ topic, event, options });
+  }
+
+  /**
+   * Publishes all buffered events to the {@link BufferEventTransaction.publisher}.
+   */
+  async commit(): Promise<void> {
+    await Promise.all(
+      this.bufferedEvents.map(({ topic, event, options }) =>
+        this.publisher.publish(topic, event, options),
+      ),
+    );
+  }
+}
--- src/transaction/index.ts
+++ src/transaction/index.ts
@@ -1,1 +1,2 @@
+export { BufferEventTransaction } from './buffer-event-transaction.js';
 export * from './event-transaction.js';
