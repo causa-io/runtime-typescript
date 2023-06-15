e28ff7c423e7f32f2e4cbf02e524b7fba594d0bf
Flo <florent.vouin@gmail.com>
2023-06-15 17:52:34 +0200

    ✨ Implement the IsGreaterThanProperty decorator

--- src/validation/decorators/index.ts
+++ src/validation/decorators/index.ts
@@ -8,6 +8,10 @@ export {
   HasUniqueValues,
   hasUniqueValues,
 } from './has-unique-values.decorator.js';
+export {
+  IsGreaterThanProperty,
+  isGreaterThanValue,
+} from './is-greater-than-property.decorator.js';
 export { IsNullable } from './is-nullable.decorator.js';
 export { RequiresPropertyNotToBeDefined } from './requires-property-not-to-be-defined.decorator.js';
 export { RequiresProperty } from './requires-property.decorator.js';
---
+++ src/validation/decorators/is-greater-than-property.decorator.spec.ts
@@ -0,0 +1,77 @@
+import { IsNumber, validate } from 'class-validator';
+import 'jest-extended';
+import { AllowMissing } from './allow-missing.decorator.js';
+import { IsGreaterThanProperty } from './is-greater-than-property.decorator.js';
+
+class MyObject {
+  constructor(data?: Partial<MyObject>) {
+    Object.assign(this, data);
+  }
+
+  @IsGreaterThanProperty('otherProperty')
+  property!: number;
+
+  @IsNumber()
+  @AllowMissing()
+  otherProperty?: number;
+
+  @AllowMissing()
+  @IsGreaterThanProperty('optionalProperty', { requireOtherProperty: false })
+  someOtherGreatProperty?: number;
+
+  @IsNumber()
+  @AllowMissing()
+  optionalProperty?: number;
+}
+
+describe('IsGreaterThanProperty', () => {
+  it('should validate when the decorated property is greater than the other property', async () => {
+    const obj = new MyObject({ property: 2, otherProperty: 1 });
+
+    const errors = await validate(obj);
+
+    expect(errors).toBeEmpty();
+  });
+
+  it('should not validate when the decorated property is equal to the other property', async () => {
+    const obj = new MyObject({ property: 1, otherProperty: 1 });
+
+    const errors = await validate(obj);
+
+    expect(errors).toEqual([
+      expect.objectContaining({
+        constraints: {
+          isGreaterThanProperty: `'property' should be greater than 'otherProperty'.`,
+        },
+        property: 'property',
+      }),
+    ]);
+  });
+
+  it('should not validate when the other property is missing', async () => {
+    const obj = new MyObject({ property: 1 });
+
+    const errors = await validate(obj);
+
+    expect(errors).toEqual([
+      expect.objectContaining({
+        constraints: {
+          isGreaterThanProperty: `'property' should be greater than 'otherProperty'.`,
+        },
+        property: 'property',
+      }),
+    ]);
+  });
+
+  it('should validate when the other property is not required', async () => {
+    const obj = new MyObject({
+      property: 2,
+      otherProperty: 1,
+      someOtherGreatProperty: 10,
+    });
+
+    const errors = await validate(obj);
+
+    expect(errors).toBeEmpty();
+  });
+});
