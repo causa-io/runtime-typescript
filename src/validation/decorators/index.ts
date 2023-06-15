50493f2c45322d234b7baea6d8fa381df99ab9db
Flo <florent.vouin@gmail.com>
2023-06-15 16:49:05 +0200

    ✨ Implement the RequiresPropertyNotToBeDefined decorator

--- src/validation/decorators/index.ts
+++ src/validation/decorators/index.ts
@@ -8,4 +8,5 @@ export {
   hasUniqueValues,
 } from './has-unique-values.decorator.js';
 export { IsNullable } from './is-nullable.decorator.js';
+export { RequiresPropertyNotToBeDefined } from './requires-property-not-to-be-defined.decorator.js';
 export { RequiresProperty } from './requires-property.decorator.js';
