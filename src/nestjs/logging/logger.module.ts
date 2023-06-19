594e57aa83c4d80eae6ee380af23afa6be9108b8
Flo <florent.vouin@gmail.com>
2023-06-19 18:06:07 +0200

    ✨ Implement the LoggerModule

---
+++ src/nestjs/logging/logger.module.ts
@@ -0,0 +1,24 @@
+import { Global, Logger, Module } from '@nestjs/common';
+import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+import { getDefaultLogger } from '../../logging/index.js';
+
+/**
+ * This module exposes a ready-to-use logger for NestJS REST services. It can be imported in the application module such
+ * that the `Logger` dependency can be injected into any module.
+ */
+@Global()
+@Module({
+  imports: [
+    PinoLoggerModule.forRoot({
+      pinoHttp: {
+        // Passing the default logger ensures the default configuration is inherited...
+        logger: getDefaultLogger(),
+        // ... the following only sets up pino-http specific settings.
+        redact: { paths: ['req.headers.authorization'] },
+      },
+    }),
+  ],
+  providers: [Logger],
+  exports: [Logger],
+})
+export class LoggerModule {}
