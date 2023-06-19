ea78e0a3db98cbaea34172cbc7c4647a75749e2f
Flo <florent.vouin@gmail.com>
2023-06-19 17:48:13 +0200

    ✨ Implement the makeTestAppFactory testing utility

--- src/nestjs/factory/testing.spec.ts
+++ src/nestjs/factory/testing.spec.ts
@@ -1,6 +1,98 @@
-import { createMockConfigService } from './testing.js';
+import {
+  Controller,
+  INestApplication,
+  Injectable,
+  Module,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createApp } from './app-factory.js';
+import { createMockConfigService, makeTestAppFactory } from './testing.js';
+
+@Injectable()
+class MyService {
+  computeStuff() {
+    return '➗';
+  }
+}
+
+@Controller('test')
+class TestController {
+  readonly configValue: string;
+  readonly serviceOutput: string;
+
+  constructor(configService: ConfigService, myService: MyService) {
+    this.configValue = configService.getOrThrow('MY_VAR');
+    this.serviceOutput = myService.computeStuff();
+  }
+}
+
+@Module({ controllers: [TestController], providers: [MyService] })
+class TestModule {}
+
+@Module({
+  imports: [TestModule],
+})
+class AppModule {}
 
 describe('test-utils', () => {
+  describe('makeTestAppFactory', () => {
+    it('should use the provided config dictionary', async () => {
+      let app!: INestApplication;
+      let actualConfigValue: string;
+      try {
+        app = await createApp(AppModule, {
+          appFactory: makeTestAppFactory({ config: { MY_VAR: '🎉' } }),
+        });
+        const actualController = app.get(TestController);
+        actualConfigValue = actualController.configValue;
+      } finally {
+        await app?.close();
+      }
+
+      expect(actualConfigValue).toEqual('🎉');
+    });
+
+    it('use the provided config service', async () => {
+      let app!: INestApplication;
+      let actualConfigValue: string;
+      try {
+        app = await createApp(AppModule, {
+          appFactory: makeTestAppFactory({
+            config: createMockConfigService({ MY_VAR: '🍦' }),
+          }),
+        });
+        const actualController = app.get(TestController);
+        actualConfigValue = actualController.configValue;
+      } finally {
+        await app?.close();
+      }
+
+      expect(actualConfigValue).toEqual('🍦');
+    });
+
+    it('should allow overriding providers', async () => {
+      let app!: INestApplication;
+      let actualServiceOutput: string;
+      try {
+        app = await createApp(AppModule, {
+          appFactory: makeTestAppFactory({
+            config: { MY_VAR: '🍦' },
+            overrides: (builder) =>
+              builder
+                .overrideProvider(MyService)
+                .useValue({ computeStuff: () => '🤡' }),
+          }),
+        });
+        const actualController = app.get(TestController);
+        actualServiceOutput = actualController.serviceOutput;
+      } finally {
+        await app?.close();
+      }
+
+      expect(actualServiceOutput).toEqual('🤡');
+    });
+  });
+
   describe('createMockConfigService', () => {
     it('should return the initial configuration', () => {
       const configService = createMockConfigService({
--- src/nestjs/factory/testing.ts
+++ src/nestjs/factory/testing.ts
@@ -1,5 +1,8 @@
 import { jest } from '@jest/globals';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Test, TestingModuleBuilder } from '@nestjs/testing';
+import { AppFactory } from './app-factory.js';
 
 /**
  * A mocked {@link ConfigService}.
@@ -35,4 +38,55 @@ export function createMockConfigService(
     .mockReturnValue(undefined);
 
   return configService as any;
+}
+
+/**
+ * The options for the {@link makeTestAppFactory} function.
+ */
+export type MakeTestAppFactoryOptions = {
+  /**
+   * A dictionary of configuration values (e.g. environment variables), or a (mocked) config service that will be
+   * injected as a replacement to the original {@link ConfigService}.
+   */
+  config?: Record<string, any> | ConfigService | MockedConfigService;
+
+  /**
+   * A function that overrides providers in the passed module builder.
+   *
+   * @param builder The module builder.
+   * @returns The module builder returned by calls to builder methods.
+   */
+  overrides?: (builder: TestingModuleBuilder) => TestingModuleBuilder;
+};
+
+/**
+ * Makes a new {@link AppFactory} which is based on a testing module.
+ * By default, at least the {@link ConfigService} is mocked, but other providers can be replaced as well.
+ *
+ * @param options Options when making the factory.
+ * @returns The factory that can be passed to `createApp`.
+ */
+export function makeTestAppFactory(
+  options: MakeTestAppFactoryOptions = {},
+): AppFactory {
+  let configService: ConfigService | MockedConfigService;
+  if (options.config instanceof ConfigService) {
+    configService = options.config;
+  } else {
+    configService = createMockConfigService(options.config ?? process.env);
+  }
+
+  return async (appModule) => {
+    let builder = Test.createTestingModule({ imports: [appModule] })
+      .overrideProvider(ConfigService)
+      .useValue(configService);
+
+    if (options.overrides) {
+      builder = options.overrides(builder);
+    }
+
+    const moduleRef = await builder.compile();
+
+    return moduleRef.createNestApplication<NestExpressApplication>();
+  };
 }
