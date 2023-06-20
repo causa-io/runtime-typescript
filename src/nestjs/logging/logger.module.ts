2b49669daa7f32e2b39c3fd8c6141035c78a0922
Flo <florent.vouin@gmail.com>
2023-06-20 07:27:28 +0200

    🔇 Do not log successful healthcheck requests

--- src/nestjs/logging/logger.module.ts
+++ src/nestjs/logging/logger.module.ts
@@ -1,6 +1,7 @@
 import { Global, Logger, Module } from '@nestjs/common';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 import { getDefaultLogger } from '../../logging/index.js';
+import { HEALTH_ENDPOINT } from '../healthcheck/healthcheck.module.js';
 
 /**
  * This module exposes a ready-to-use logger for NestJS REST services. It can be imported in the application module such
@@ -14,6 +15,9 @@ import { getDefaultLogger } from '../../logging/index.js';
         // Passing the default logger ensures the default configuration is inherited...
         logger: getDefaultLogger(),
         // ... the following only sets up pino-http specific settings.
+        autoLogging: {
+          ignore: (req) => (req as any).originalUrl === `/${HEALTH_ENDPOINT}`,
+        },
         redact: { paths: ['req.headers.authorization'] },
       },
     }),
