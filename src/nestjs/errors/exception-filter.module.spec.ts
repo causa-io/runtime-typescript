98c5928cd00e05134cefa6f35524ec22e16e522b
Flo <florent.vouin@gmail.com>
2023-06-20 10:47:33 +0200

    ✨ Implement the ExceptionFilterModule

---
+++ src/nestjs/errors/exception-filter.module.spec.ts
@@ -0,0 +1,163 @@
+import { Controller, Get, HttpStatus, INestApplication } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Test } from '@nestjs/testing';
+import 'jest-extended';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import supertest from 'supertest';
+import {
+  EntityAlreadyExistsError,
+  EntityNotFoundError,
+  IncorrectEntityVersionError,
+} from '../../errors/index.js';
+import { getLoggedErrors, spyOnLogger } from '../../logging/testing.js';
+import { LoggerModule } from '../logging/index.js';
+import { ExceptionFilterModule } from './exception-filter.module.js';
+import { ErrorResponse, HttpError } from './http-error.js';
+
+class MyErrorDto implements ErrorResponse {
+  readonly statusCode = HttpStatus.I_AM_A_TEAPOT;
+  readonly message = '🫖';
+  readonly errorCode = 'teapot';
+}
+
+class MyError extends HttpError<MyErrorDto> {
+  constructor() {
+    super(new MyErrorDto());
+  }
+}
+
+@Controller('/')
+class TestController {
+  @Get('/VersionNotMatchingError')
+  async notMatching() {
+    throw new IncorrectEntityVersionError(
+      {} as any,
+      {},
+      new Date(),
+      new Date(),
+    );
+  }
+
+  @Get('/EntityNotFoundError')
+  async notFound() {
+    throw new EntityNotFoundError({} as any, {});
+  }
+
+  @Get('/EntityAlreadyExistsError')
+  async alreadyExists() {
+    throw new EntityAlreadyExistsError({} as any, {});
+  }
+
+  @Get('/InternalServerError')
+  async internalServerError() {
+    throw new Error('💥');
+  }
+
+  @Get('/CustomError')
+  async customError() {
+    throw new MyError();
+  }
+}
+
+describe('ExceptionFilterModule', () => {
+  let app: INestApplication;
+  let request: supertest.SuperTest<supertest.Test>;
+
+  beforeEach(async () => {
+    spyOnLogger();
+
+    const testingModule = await Test.createTestingModule({
+      imports: [ExceptionFilterModule, LoggerModule],
+      controllers: [TestController],
+    }).compile();
+    app = testingModule.createNestApplication<NestExpressApplication>();
+    app.useLogger(app.get(PinoLogger));
+    await app.init();
+    request = supertest(app.getHttpServer());
+  });
+
+  it('should return 409 when an EntityAlreadyExistsError is thrown', async () => {
+    await request
+      .get('/VersionNotMatchingError')
+      .expect(409)
+      .expect(({ body }) => {
+        expect(body).toEqual({
+          statusCode: 409,
+          message:
+            'The provided version does not match the version of the resource on the server.',
+          errorCode: 'incorrectVersion',
+        });
+      });
+
+    expect(getLoggedErrors()).toBeEmpty();
+  });
+
+  it('should return 409 when an EntityAlreadyExistsError is thrown', async () => {
+    await request
+      .get('/EntityAlreadyExistsError')
+      .expect(409)
+      .expect(({ body }) => {
+        expect(body).toEqual({
+          statusCode: 409,
+          message:
+            'The request conflicts with existing resource(s) on the server.',
+          errorCode: 'conflict',
+        });
+      });
+
+    expect(getLoggedErrors()).toBeEmpty();
+  });
+
+  it('should return 404 when an EntityNotFoundError is thrown', async () => {
+    await request
+      .get('/EntityNotFoundError')
+      .expect(404)
+      .expect(({ body }) => {
+        expect(body).toEqual({
+          statusCode: 404,
+          message: 'The requested resource was not found on the server.',
+          errorCode: 'notFound',
+        });
+      });
+
+    expect(getLoggedErrors()).toBeEmpty();
+  });
+
+  it('should return 500 with the correct format', async () => {
+    await request
+      .get('/InternalServerError')
+      .expect(500)
+      .expect(({ body }) => {
+        expect(body).toEqual({
+          statusCode: 500,
+          message: 'An unexpected error occurred on the server.',
+          errorCode: 'internalServerError',
+        });
+      });
+
+    expect(getLoggedErrors({ predicate: (o) => o.message === '💥' })).toEqual([
+      expect.objectContaining({
+        req: expect.objectContaining({ url: '/InternalServerError' }),
+        err: expect.objectContaining({
+          message: '💥',
+          stack: expect.stringContaining('💥'),
+        }),
+      }),
+    ]);
+  });
+
+  it('should not convert a custom HTTP error to an InternalServerError', async () => {
+    await request
+      .get('/CustomError')
+      .expect(418)
+      .expect(({ body }) => {
+        expect(body).toEqual({
+          statusCode: 418,
+          message: '🫖',
+          errorCode: 'teapot',
+        });
+      });
+
+    expect(getLoggedErrors()).toBeEmpty();
+  });
+});
--- src/nestjs/errors/index.ts
+++ src/nestjs/errors/index.ts
@@ -1,2 +1,3 @@
 export * from './errors.dto.js';
+export { ExceptionFilterModule } from './exception-filter.module.js';
 export * from './http-error.js';
