88723e994819eb70e8632095068c2c02e5c8b22c
Flo <florent.vouin@gmail.com>
2023-06-22 09:54:06 +0200

    ✨ Implement the AuthUser decorator

---
+++ src/nestjs/auth/auth-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '../../auth/index.js';
+
+/**
+ * Decorator for controller methods that fetch the {@link User} object from the request.
+ */
+export const AuthUser = createParamDecorator(
+  (_: unknown, ctx: ExecutionContext): User => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
