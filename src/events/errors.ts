c6e8ef8da7799ad4481c10b457ce9f87fb06422d
Flo <florent.vouin@gmail.com>
2023-06-23 11:16:34 +0200

    ✨ Define the InvalidEventError

---
+++ src/events/errors.ts
@@ -0,0 +1,4 @@
+/**
+ * An error thrown when the event to publish or parse does not have the expected format.
+ */
+export class InvalidEventError extends Error {}
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,2 +1,3 @@
+export * from './errors.js';
 export * from './event.js';
 export * from './publisher.js';
