d30edc66614335696b989f02785a1f5c55c83533
Flo <florent.vouin@gmail.com>
2023-09-13 13:52:19 +0200

    ✨ Implement the ApiErrorResponses decorator

---
+++ src/nestjs/openapi/api-error-responses.decorator.spec.ts
@@ -0,0 +1,129 @@
+import { Controller, Get, HttpStatus, Module } from '@nestjs/common';
+import {
+  ApiErrorDescription,
+  ApiErrorResponses,
+  ApiErrorStatusCode,
+  ErrorDto,
+} from '../index.js';
+import { generateOpenApiDocument } from './utils.test.js';
+
+@ApiErrorDescription('📚')
+class MyError1Dto extends ErrorDto {
+  @ApiErrorStatusCode(HttpStatus.I_AM_A_TEAPOT)
+  readonly statusCode = HttpStatus.I_AM_A_TEAPOT;
+}
+
+@ApiErrorDescription('📙')
+class MyError2Dto extends ErrorDto {
+  @ApiErrorStatusCode(HttpStatus.BAD_REQUEST)
+  readonly statusCode = HttpStatus.BAD_REQUEST;
+}
+
+@ApiErrorDescription('📗')
+class MyError3Dto extends ErrorDto {
+  @ApiErrorStatusCode(HttpStatus.BAD_REQUEST)
+  readonly statusCode = HttpStatus.BAD_REQUEST;
+}
+
+@ApiErrorDescription('📕')
+class MyError4Dto extends ErrorDto {
+  @ApiErrorStatusCode(HttpStatus.NOT_FOUND)
+  readonly statusCode = HttpStatus.NOT_FOUND;
+}
+
+@Controller()
+@ApiErrorResponses(MyError1Dto)
+class MyController {
+  @Get()
+  @ApiErrorResponses(MyError2Dto, MyError3Dto, MyError4Dto)
+  get(): void {}
+}
+
+@Module({ controllers: [MyController] })
+class MyModule {}
+
+describe('ApiErrorResponses', () => {
+  it('should document the endpoints', async () => {
+    const actualDocument = await generateOpenApiDocument(MyModule);
+    const actualGetPath = actualDocument.paths['/'].get;
+    const actualSchemas = actualDocument.components?.schemas;
+
+    expect(actualGetPath?.responses['400']).toMatchObject({
+      content: {
+        'application/json': {
+          schema: {
+            oneOf: [
+              { $ref: '#/components/schemas/MyError2Dto' },
+              { $ref: '#/components/schemas/MyError3Dto' },
+            ],
+          },
+        },
+      },
+      description: '- 📙\n- 📗',
+    });
+    expect(actualGetPath?.responses['404']).toMatchObject({
+      content: {
+        'application/json': {
+          schema: { oneOf: [{ $ref: '#/components/schemas/MyError4Dto' }] },
+        },
+      },
+      description: '📕',
+    });
+    expect(actualGetPath?.responses['418']).toMatchObject({
+      content: {
+        'application/json': {
+          schema: { oneOf: [{ $ref: '#/components/schemas/MyError1Dto' }] },
+        },
+      },
+      description: '📚',
+    });
+    expect(actualSchemas?.MyError1Dto).toMatchObject({
+      type: 'object',
+      properties: {
+        statusCode: {
+          example: 418,
+          const: 418,
+          type: 'number',
+          description: expect.any(String),
+        },
+      },
+      required: ['statusCode', 'message', 'errorCode'],
+    });
+    expect(actualSchemas?.MyError2Dto).toMatchObject({
+      type: 'object',
+      properties: {
+        statusCode: {
+          example: 400,
+          const: 400,
+          type: 'number',
+          description: expect.any(String),
+        },
+      },
+      required: ['statusCode', 'message', 'errorCode'],
+    });
+    expect(actualSchemas?.MyError3Dto).toMatchObject({
+      type: 'object',
+      properties: {
+        statusCode: {
+          example: 400,
+          const: 400,
+          type: 'number',
+          description: expect.any(String),
+        },
+      },
+      required: ['statusCode', 'message', 'errorCode'],
+    });
+    expect(actualSchemas?.MyError4Dto).toMatchObject({
+      type: 'object',
+      properties: {
+        statusCode: {
+          example: 404,
+          const: 404,
+          type: 'number',
+          description: expect.any(String),
+        },
+      },
+      required: ['statusCode', 'message', 'errorCode'],
+    });
+  });
+});
---
+++ src/nestjs/openapi/api-error-responses.decorator.ts
@@ -0,0 +1,53 @@
+import { Type } from '@nestjs/common';
+import { ApiExtraModels, ApiResponse, refs } from '@nestjs/swagger';
+import { ErrorDto } from '../index.js';
+import { getApiErrorDescription } from './api-error-description.decorator.js';
+import { getApiErrorStatusCode } from './api-error-status-code.decorator.js';
+
+/**
+ * Decorates a controller method or class with the given error DTOs.
+ * This documents the endpoint(s) to indicate that they can return those DTOs in the OpenAPI specification.
+ *
+ * A current limitation is that errors set a the class level will be overridden by errors set at the method level _for
+ * the same status code_.
+ *
+ * @param dtos The error DTOs that can be returned by the endpoint.
+ */
+export function ApiErrorResponses(
+  ...dtos: { new (...args: any[]): ErrorDto }[]
+): MethodDecorator & ClassDecorator {
+  const errorsByStatus = dtos.reduce(
+    (responses, type) => {
+      const statusCode = getApiErrorStatusCode(type);
+      const description = getApiErrorDescription(type);
+
+      responses[statusCode] = [
+        ...(responses[statusCode] || []),
+        { description, type },
+      ];
+      return responses;
+    },
+    {} as Record<number, { description: string; type: Type }[]>,
+  );
+
+  return (
+    target: object,
+    propertyKey?: string | symbol,
+    descriptor?: TypedPropertyDescriptor<any>,
+  ) => {
+    ApiExtraModels(...dtos)(target, propertyKey, descriptor);
+
+    Object.entries(errorsByStatus).forEach(([status, errors]) => {
+      const description =
+        errors.length > 1
+          ? errors.map((e) => `- ${e.description}`).join('\n')
+          : errors[0].description;
+
+      ApiResponse({
+        status: parseInt(status),
+        description,
+        schema: { oneOf: refs(...errors.map((e) => e.type)) },
+      })(target, propertyKey as any, descriptor as any);
+    });
+  };
+}
