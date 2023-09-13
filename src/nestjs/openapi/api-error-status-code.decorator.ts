f26d17751cd60c62428071af9348ab3e224fd0f8
Flo <florent.vouin@gmail.com>
2023-09-13 12:06:05 +0200

    ✨ Implement the ApiErrorStatusCode decorator

---
+++ src/nestjs/openapi/api-error-status-code.decorator.ts
@@ -0,0 +1,39 @@
+import { ApiConstantProperty } from '@causa/runtime/nestjs';
+import { HttpStatus, Type } from '@nestjs/common';
+import { ErrorDto } from '../errors/index.js';
+
+/**
+ * The metadata key used to store the API error status code.
+ */
+const API_ERROR_STATUS_CODE_KEY = 'apiErrorStatusCode';
+
+/**
+ * Decorates the `statusCode` property of an {@link ErrorDto} with the given status code.
+ * This is needed by the `ApiErrorResponses` decorator to generate the OpenAPI documentation.
+ * This decorator also adds the {@link ApiConstantProperty} decorator to the property.
+ *
+ * @param code The status code for the property.
+ */
+export function ApiErrorStatusCode<T extends HttpStatus>(code: T) {
+  const apiConstantProperty = ApiConstantProperty({ const: code });
+
+  return (target: ErrorDto & { statusCode: T }, propertyKey: 'statusCode') => {
+    Reflect.defineMetadata(API_ERROR_STATUS_CODE_KEY, code, target.constructor);
+    apiConstantProperty(target, propertyKey);
+  };
+}
+
+/**
+ * Retrieves the {@link ErrorDto.statusCode} of a property decorated with {@link ApiErrorStatusCode}.
+ *
+ * @param type The type of the error DTO.
+ * @returns The status code of the error DTO.
+ */
+export function getApiErrorStatusCode(type: Type): number {
+  const statusCode = Reflect.getMetadata(API_ERROR_STATUS_CODE_KEY, type);
+  if (statusCode === undefined) {
+    throw new Error(`No API error status code found for '${type.name}'.`);
+  }
+
+  return statusCode;
+}
--- src/nestjs/openapi/index.ts
+++ src/nestjs/openapi/index.ts
@@ -1,1 +1,2 @@
 export { ApiConstantProperty } from './api-constant-property.decorator.js';
+export { ApiErrorStatusCode } from './api-error-status-code.decorator.js';
