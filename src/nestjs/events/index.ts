5a1b7cbc62e08b827cedb69adc9bcded11f4a207
Flo <florent.vouin@gmail.com>
2023-12-08 17:22:52 +0100

    ✨ Define the UseEventHandler decorator

--- src/nestjs/events/index.ts
+++ src/nestjs/events/index.ts
@@ -6,3 +6,4 @@ export * from './event-attributes.decorator.js';
 export * from './event-body.decorator.js';
 export * from './publisher.decorator.js';
 export * from './request-with-event.js';
+export { UseEventHandler } from './use-event-handler.decorator.js';
