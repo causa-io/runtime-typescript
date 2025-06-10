e0f7f5f7041c6256550025075f78eea0d4f559bd
Flo <florent.vouin@gmail.com>
2025-06-10 21:49:05 +0200

    💥 Rename FindReplaceStateTransaction to StateTransaction and use get, set, delete terminology

--- src/transaction/state-transaction.ts
+++ src/transaction/state-transaction.ts
@@ -1,20 +1,17 @@
 import type { Type } from '@nestjs/common';
-import type { EventTransaction } from './event-transaction.js';
-import { Transaction } from './transaction.js';
 
 /**
- * A transaction that exposes methods to find and replace entities based on their primary key.
+ * A transaction that exposes methods to get and set entities based on their primary key.
  */
-export interface FindReplaceStateTransaction {
+export interface StateTransaction {
   /**
    * Replaces a possibly existing entity with the given one.
    * If the entity does not exist, it is created.
-   * If the entity exists, it is fully replaced, not just updated. This means that unspecified properties are set to
-   * `null` rather than being left untouched.
+   * If the entity exists, it is fully replaced, not just updated.
    *
    * @param entity The entity to set in the state.
    */
-  replace<T extends object>(entity: T): Promise<void>;
+  set<T extends object>(entity: T): Promise<void>;
 
   /**
    * Deletes an entity of the given type with the same primary key as the given entity.
@@ -22,30 +19,19 @@ export interface FindReplaceStateTransaction {
    * @param type The type of the entity to delete.
    * @param key The primary key of the entity to delete, as a partial entity containing all the primary key columns.
    */
-  deleteWithSameKeyAs<T extends object>(
-    type: Type<T>,
-    key: Partial<T>,
-  ): Promise<void>;
+  delete<T extends object>(type: Type<T>, key: Partial<T>): Promise<void>;
+  delete<T extends object>(entity: T): Promise<void>;
 
   /**
    * Looks up an entity of the given type with the same primary key as the given entity.
-   * This should return the entity even if it is deleted.
    *
    * @param type The type of the entity to find.
    * @param entity The entity to use as a template for the search. All the primary key should be set, and other
    *   properties will be ignored.
    * @returns The entity if it exists, or `undefined` if it does not.
    */
-  findOneWithSameKeyAs<T extends object>(
+  get<T extends object>(
     type: Type<T>,
     entity: Partial<T>,
   ): Promise<T | undefined>;
 }
-
-/**
- * A transaction that at least conforms to {@link FindReplaceStateTransaction}.
- */
-export type FindReplaceTransaction = Transaction<
-  FindReplaceStateTransaction,
-  EventTransaction
->;
